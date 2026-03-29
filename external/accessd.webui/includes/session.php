6dd7048de8f633f2716540671d21d35f1d1ac24e
HalfSilver <69383504+Half-Silver@users.noreply.github.com>
2026-03-29 19:55:54 +0530

    Ensure writable paths for WebUI logging/cache and make session start resilient

--- external/accessd.webui/includes/session.php
+++ external/accessd.webui/includes/session.php
@@ -1,10 +1,10 @@
 <?php
 
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
+if (session_status() === PHP_SESSION_NONE && !headers_sent()) {
+    @session_start();
 }
 
-if (!isset($_SESSION['lastActivity'])) {
+if (isset($_SESSION) && !isset($_SESSION['lastActivity'])) {
     $_SESSION['lastActivity'] = time();
 }
 
